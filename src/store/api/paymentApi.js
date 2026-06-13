13c1d587349d01119d183742a949eb673e3e87d2
Nombre Apellido <be.marinv@duocuc.cl>
2026-06-13 05:52:58 -0400

    fix: correcion de URL
    para pruebas locales de payment api

--- src/store/api/paymentApi.js
+++ src/store/api/paymentApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const paymentApi = createApi({
     reducerPath: "paymentApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://ap-916a8432b2994023864bb12867d8c2df.ecs.sa-east-1.on.aws",
+        baseUrl: "http://localhost:3000",
         credentials: "include",
         prepareHeaders: (headers) => {
             headers.set("Content-Type", "application/json");
