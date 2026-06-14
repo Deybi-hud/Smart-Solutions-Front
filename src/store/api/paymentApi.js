581dedded530c86a4ad5a49cbf42eea02142b8cc
Deybi-hud <deyb.ramirez@duocuc.cl>
2026-06-14 02:48:59 -0400

    feat: Implementación de baseurl aputando dns de api-payment

--- src/store/api/paymentApi.js
+++ src/store/api/paymentApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const paymentApi = createApi({
     reducerPath: "paymentApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://smartsolutions-payment-alb-42793780.sa-east-1.elb.amazonaws.com",
+        baseUrl: "https://ap-916a8432b2994023864bb12867d8c2df.ecs.sa-east-1.on.aws",
         credentials: "include",
         prepareHeaders: (headers) => {
             headers.set("Content-Type", "application/json");
